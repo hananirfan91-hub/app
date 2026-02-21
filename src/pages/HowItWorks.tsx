56a78cc4d0740952d9bcfd25af2b8f567cd0341b
hananirfan91-hub <hananirfan91@gmail.com>
2026-02-21 09:18:21 +0500

    Update HowItWorks.tsx

--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -319,7 +319,7 @@ export default function HowItWorks() {
               <iframe
                 width="100%"
                 height="100%"
-                src="https://youtu.be/D_28Hn4azWc?si=x7q2VDONX3I-R-Gg"
+                src="https://www.youtube.com/embed/D_28Hn4azWc?si=i1yIE-xWt6N5mb_n"
                 title="How We Work at Hanan Irfan Digital Group - Our 5-Step Process"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
